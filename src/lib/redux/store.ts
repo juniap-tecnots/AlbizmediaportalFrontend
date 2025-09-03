e3fc076034a5264c5abf2bf840c76fa0f1137e37
Juniya P <juniyap47@gmail.com>
2025-09-03 03:33:15 +0000

    When I add a new article, it gets added and listed correctly, but after

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,11 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import articlesReducer from './slices/articlesSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  articles: articlesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    articles: articlesReducer,
-  },
+  reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
