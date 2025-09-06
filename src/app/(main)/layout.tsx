5f6dba9542c447d843ebedbe05343717fc1d8c02
Juniya P <juniyap47@gmail.com>
2025-09-06 14:36:15 +0000

    navbar need a background color

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,9 +9,9 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <AdminSidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-64">
+      <div className="flex flex-col sm:pl-64">
         <Header />
-        <main className="flex-1">
+        <main className="flex-1 p-6 md:p-8">
           {children}
         </main>
       </div>
