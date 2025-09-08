77b974712e64e1d6a818cfb984e5dffef24d3b40
Juniya P <juniyap47@gmail.com>
2025-09-08 07:11:55 +0000

    here in the sidebar menu icon svg height="16" width="16" so change to 20

--- src/components/admin-sidebar.tsx
+++ src/components/admin-sidebar.tsx
@@ -147,7 +147,7 @@ export function AdminSidebar() {
                             'text-sidebar-foreground group-hover:text-sidebar-accent-foreground',
                              active && 'text-primary-foreground'
                         )}>
-                            <item.icon size={16} />
+                            <item.icon size={20} />
                         </span>
                         <span className="font-medium text-sm truncate">
                             {item.label}
